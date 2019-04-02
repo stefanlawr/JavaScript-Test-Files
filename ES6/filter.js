fc135ba0ce141d2b3d5a17615196f1cb775a68da
Stefan Strauch <sstrauch@ups.com>
2019-04-02 13:25:01 -0400

    Updated var to let

--- ES6/filter.js
+++ ES6/filter.js
@@ -1,12 +1,29 @@
 // A Filter creates a new filtered array from an array passed into the function
 
-var animals = [
-	{ name: 'Fluffle', species: 'rabbit' },
-	{ name: 'Doggo', species: 'dog' },
-	{ name: 'Woofer', species: 'dog' },
-	{ name: 'Fishy Boi', species: 'fish' },
-	{ name: 'Bob', species: 'cat' },
-	{ name: 'Bubble', species: 'fish' },
+let animals = [{
+    name: 'Fluffle',
+    species: 'rabbit'
+  },
+  {
+    name: 'Doggo',
+    species: 'dog'
+  },
+  {
+    name: 'Woofer',
+    species: 'dog'
+  },
+  {
+    name: 'Fishy Boi',
+    species: 'fish'
+  },
+  {
+    name: 'Bob',
+    species: 'cat'
+  },
+  {
+    name: 'Bubble',
+    species: 'fish'
+  },
 ];
 
 let dogs1 = [];
@@ -14,9 +31,9 @@ let dogs1 = [];
 // So this foor loop does the same thing as
 // For each loop
 for (let animal of animals) {
-	if (animal.species === 'dog') {
-		dogs1.push(animal);
-	}
+  if (animal.species === 'dog') {
+    dogs1.push(animal);
+  }
 }
 
 // This filter statement
